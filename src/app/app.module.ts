bfc9ceabab69621a07056ec848288f3af86f4aa1
acharayaP03 <acharyaP03@gmail.com>
2020-10-29 21:17:39 +1100

    usign angular pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ArtistItemsComponent } from './artist-items/artist-items.component';
+import { SearchArtistPipe } from './search-artist.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArtistItemsComponent
+    ArtistItemsComponent,
+    SearchArtistPipe
   ],
   imports: [
     BrowserModule,  HttpClientModule, FormsModule,
